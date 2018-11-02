92859c737e5a7eacdca7003952b585799cbecac1
wayneasilva <wayneasilva@gmail.com>
2018-11-02 17:51:37 -0400

    First Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("CONNECTED");
+
+//declare document variable and set it to the selected element
+const MY_DOCUMENT = document.querySelector("#bill-of-sale");
+const PRINT_BUTTON = document.querySelector("#print-button");
+
+//add event listener to button
+PRINT_BUTTON.addEventListener("click", function() {
+    window.print(MY_DOCUMENT);
+})
